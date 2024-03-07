a02d4d5edf399ab3360ebb57fb39f25556ec6dad
Duc Phan <dphan@esri.com>
2024-03-07 10:59:23 -0800

    fix script, add more script

---
+++ random-sum.js
@@ -0,0 +1,9 @@
+const sum = (a, b) => a + b;
+
+const a = Math.floor(Math.random() * 10);
+const b = Math.floor(Math.random() * 10);
+const result = sum(a, b);
+
+console.log(`
+  Yo dawg I heard you like random numbers so I put a random number in your random number so you can random while you random ${a} + ${b} = ${result}.
+`);
